f9bfb8353a102353b83d4e8316a562e1d6c9a4c3
Paweł Kochowicz <pawel.kochowicz@gmail.com>
2020-04-12 16:32:36 +0200

    feat:create function to add task to list

--- src/js/main.js
+++ src/js/main.js
@@ -6,3 +6,14 @@ const addButton = document.querySelector('.addTaskButton');
 const taskCounter = document.querySelector('.taskCounter');
 const input = document.querySelector('.taskInput');
 
+const addTask = (e) => {
+e.preventDefault();
+const taskTitle = input.value;
+if (taskTitle === "") return;
+const task = document.createElement('li');
+task.className = 'task';
+task.innerHTML = taskTitle + "<button> Usuń</button>";
+ul.appendChild(task);
+}
+
+addButton.addEventListener('click',addTask) 
