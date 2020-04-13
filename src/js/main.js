19138c0e915826fbf6aff75a84dcbc929e8a7fe8
Paweł Kochowicz <pawel.kochowicz@gmail.com>
2020-04-13 18:28:43 +0200

    feat: can add one task to localStorage

--- src/js/main.js
+++ src/js/main.js
@@ -5,24 +5,39 @@ const ul = document.querySelector('.taskList');
 const addButton = document.querySelector('.addTaskButton');
 const taskCounter = document.querySelector('.taskCounter');
 const input = document.querySelector('.taskInput');
-const listItems = document.getElementsByClassName('task')
+const listItems = document.getElementsByClassName('task');
+
+
+
+if (!localStorage.getItem(input)) {
+  taskCounter.innerHTML = '0';
+} else {
+  taskCounter.innerHTML = localStorage.getItem(input)
+}
 
 const addTask = (e) => {
-e.preventDefault();
-const taskTitle = input.value;
-if (taskTitle === "") return;
-const task = document.createElement('li');
-task.className = 'task';
-task.innerHTML = taskTitle + `<button class="removeButton"> Usuń</button>`;
-ul.appendChild(task);
-input.value = "";
-task.querySelector('.removeButton').addEventListener('click', removeTask);
-taskCounter.innerHTML = listItems.length;
+  e.preventDefault();
+  localStorage.setItem('task', input.value)
+  // taskCounter.innerHTML = localStorage.getItem(input);
+  // const task = document.createElement('li');
+  // task.className = 'task';
+  input.value = "";
+  // task.querySelector('.removeButton').addEventListener('click', removeTask);
+  // taskCounter.innerHTML = listItems.length;
+  // }
+
+  // const removeTask = (e) => {
+  // e.target.parentNode.remove();
+  // taskCounter.innerHTML = listItems.length;
+  // localStorage.removeItem("task")
 }
 
-const removeTask = (e) => {
-e.target.parentNode.remove();
-taskCounter.innerHTML = listItems.length;
-}
+addButton.addEventListener('click', addTask)
 
-addButton.addEventListener('click',addTask) 
+
+
+
+// const taskTitle = input.value;
+// if (taskTitle === "") return;
+// task.innerHTML = 
+// ul.appendChild(task);
