fcf18451045916cc90d781345c0261984b4567c0
Paweł Kochowicz <pawel.kochowicz@gmail.com>
2020-04-13 20:37:44 +0200

    feat: remove task from localStorage function

--- src/js/main.js
+++ src/js/main.js
@@ -6,32 +6,42 @@ const addButton = document.querySelector('.addTaskButton');
 const taskCounter = document.querySelector('.taskCounter');
 const input = document.querySelector('.taskInput');
 const listItems = document.getElementsByClassName('task');
+const task = document.createElement('li');
 
 
-
-if (!localStorage.getItem(input)) {
-  taskCounter.innerHTML = '0';
-} else {
-  taskCounter.innerHTML = localStorage.getItem(input)
+const start = (e) => {
+  if (!localStorage.getItem('task')) {
+    task.innerHTML = "";
+    // localStorage.setItem('task', input.value);
+  } else {
+    task.innerHTML = localStorage.getItem('task') + `<button class="removeButton">Usuń</button>`;
+    task.className = 'task';
+    ul.appendChild(task);
+    task.querySelector('.removeButton').addEventListener('click', removeTask);
+  }
 }
+
+const removeTask = (e) => {
+  e.target.parentNode.remove();
+  localStorage.removeItem("task")
+}
+
 
 const addTask = (e) => {
   e.preventDefault();
+  if (input.value === "") return;
+  task.className = 'task';
   localStorage.setItem('task', input.value)
-  // taskCounter.innerHTML = localStorage.getItem(input);
-  // const task = document.createElement('li');
-  // task.className = 'task';
+  task.innerHTML = localStorage.getItem('task') + `<button class="removeButton">Usuń</button>`;
+  // taskCounter.innerHTML = localStorage.getItem('task')
   input.value = "";
-  // task.querySelector('.removeButton').addEventListener('click', removeTask);
-  // taskCounter.innerHTML = listItems.length;
-  // }
+  ul.appendChild(task);
+  task.querySelector('.removeButton').addEventListener('click', removeTask);
 
-  // const removeTask = (e) => {
-  // e.target.parentNode.remove();
   // taskCounter.innerHTML = listItems.length;
-  // localStorage.removeItem("task")
 }
 
+start();
 addButton.addEventListener('click', addTask)
 
 
@@ -39,5 +49,3 @@ addButton.addEventListener('click', addTask)
 
 // const taskTitle = input.value;
 // if (taskTitle === "") return;
-// task.innerHTML = 
-// ul.appendChild(task);
