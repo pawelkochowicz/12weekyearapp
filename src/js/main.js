9e908df88910734f7f6f923796c1964c9a25c676
Paweł Kochowicz <pawel.kochowicz@gmail.com>
2020-04-18 23:49:16 +0200

    fix problem with delete button. Change innerHTML to textContent

--- src/js/main.js
+++ src/js/main.js
@@ -1,55 +1,39 @@
-"use strict";
+const form = document.querySelector(".taskForm");
+const ul = document.querySelector(".taskList");
+const addButton = document.querySelector(".addTaskButton");
+const taskCounter = document.querySelector(".taskCounter");
+const input = document.querySelector(".taskInput");
+let itemsArray = localStorage.getItem("items")
+  ? JSON.parse(localStorage.getItem("items"))
+  : [];
 
-const form = document.querySelector('.taskForm')
-const ul = document.querySelector('.taskList');
-const addButton = document.querySelector('.addTaskButton');
-const taskCounter = document.querySelector('.taskCounter');
-const input = document.querySelector('.taskInput');
-let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+const removeTask = (e) => {
+  const index = e.target.parentNode.remove();
+  itemsArray.splice(index, 1);
+  localStorage.setItem("items", JSON.stringify(itemsArray));
+};
 
-
-// const removeTask = (e) => {
-//   e.target.parentNode.remove();
-//   localStorage.removeItem("task")
-// }
-
-localStorage.setItem('items', JSON.stringify(itemsArray));
-const data = JSON.parse(localStorage.getItem('items'));
+localStorage.setItem("items", JSON.stringify(itemsArray));
+const data = JSON.parse(localStorage.getItem("items"));
 
 const task = (text) => {
-  const li = document.createElement('li');
-  li.innerHTML = ((text) + (`<button class="removeTask">Usuń</button>`));
+  const li = document.createElement("li");
+  li.textContent = text;
+  const button = document.createElement("button");
+  button.textContent = "usuń";
+  li.appendChild(button);
   ul.appendChild(li);
-  // task.querySelector('.removeTask').addEventListener('click', removeTask)
-}
+  button.addEventListener("click", removeTask);
+};
 
-
-
-
-form.addEventListener('submit', function (e) {
-  e.preventDefault();
-  itemsArray.push(input.value);
-  localStorage.setItem('items', JSON.stringify(itemsArray));
-  task(input.value);
-  input.value = "";
-})
-
-data.forEach(item => {
+data.forEach((item) => {
   task(item);
 });
 
-
-
-
-  // task.className = 'task';
-  // myTask.push(input.value)
-  // localStorage.setItem('task', myTask)
-  // task.innerHTML = localStorage.getItem('task') + `<button class="removeButton">Usuń</button>`;
-  // input.value = "";
-  // for (let i = 0; i < myTask.length; i++) {
-  //  const task = document.createElement('li').innerHTML = myTask[i];
-  //   ul.appendChild(task);
-  // }
-  // ul.appendChild(task);
-  // taskCounter.innerHTML = myTask.length;
-  // task.querySelector('.removeButton').addEventListener('click', removeTask);
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  itemsArray.push(input.value);
+  localStorage.setItem("items", JSON.stringify(itemsArray));
+  task(input.value);
+  input.value = "";
+});
